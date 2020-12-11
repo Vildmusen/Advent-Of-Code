c0e0dbab457546184eb4e918bc31ffe9fdeb2c3d
Viktor Vilmusenaho <viktor.vilmusenaho@ericsson.com>
2020-12-11 10:08:29 +0100

    part 2

--- scripts/[DAY 11] sneaky_seats.py
+++ scripts/[DAY 11] sneaky_seats.py
@@ -1,5 +1,7 @@
 from utils import *
 import time
+import os
+
 
 input = read_file_strings("inputs/day_11_input.txt")
 
@@ -54,13 +56,77 @@ def add_spaces(input):
 
 # Part 2
 def run_seat_cycle_v2(input):
-    input = add_spaces(input)
-    new = cycle_seats_v2(input)
+    new = cycle_seats_v3(input)
     old = input
     while(old != new):
         old = new
-        new = cycle_seats_v2(new)
+        new = cycle_seats_v3(new)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        for row in new:
+            special_row = ""
+            for char in row:
+                if(char == 'L'):
+                    special_row += '[_]'
+                if(char == '#'):
+                    special_row += '[Ö]'
+                if(char == '.'):
+                    special_row += '___'
+            print(special_row)
+        time.sleep(0.2)
     return new
+
+
+def cycle_seats_v3(input):
+    new_arr = []
+    for x, row in enumerate(input):
+        new_row = ""
+        for y, seat in enumerate(row):
+            if(seat == 'L'):
+                if(adj_seats_on_line(x,y,input) == 0):
+                    new_row += '#'
+                else:
+                    new_row += 'L'
+            elif(seat == '#'):
+                if(adj_seats_on_line(x,y,input) > 4):
+                    new_row += 'L'
+                else:
+                    new_row += '#'
+            else:
+                new_row += '.'
+        new_arr.append(new_row)
+    return new_arr
+
+
+def adj_seats_on_line(x,y,input):
+    count = 0
+    count += check_line(x,y,input,-1, 0)    # up
+    count += check_line(x,y,input,0, 1)     # right
+    count += check_line(x,y,input,1, 0)     # down
+    count += check_line(x,y,input,0, -1)    # left
+    count += check_line(x,y,input,-1, -1)   # diagonal up right
+    count += check_line(x,y,input,-1, 1)    # diagonal up left
+    count += check_line(x,y,input,1, 1)     # diagonal down right
+    count += check_line(x,y,input,1, -1)    # diagonal down left    
+    return count
+
+
+def check_line(x, y, input, dir_x, dir_y):
+    min = 0
+    x_max = len(input)
+    y_max = len(input[0])
+    dist = 1
+    x_d = (dir_x * dist)
+    y_d = (dir_y * dist)
+    while(x + x_d < x_max and x + x_d >= min and y + y_d < y_max and y + y_d >= min):
+        cur = input[x + x_d][y + y_d]
+        if(cur == '#'):
+            return 1
+        if(cur == 'L'):
+            return 0
+        dist += 1
+        x_d = (dir_x * dist)
+        y_d = (dir_y * dist)
+    return 0
 
 starttime = time.perf_counter()
 
@@ -69,13 +135,13 @@ print("\nInitial grid:")
 for row in input:
     print(row)
 
-generated_seating = run_seat_cycle(input)
-print("\nFinished grid part 1:")
-count = 0
-for row in generated_seating:
-    count += row.count('#')
-    print(row)
-print("Number of occupied seats:", count)
+# generated_seating = run_seat_cycle(input)
+# print("\nFinished grid part 1:")
+# count = 0
+# for row in generated_seating:
+#     count += row.count('#')
+#     print(row)
+# print("Number of occupied seats:", count)
 
 generated_seating = run_seat_cycle_v2(input)
 print("\nFinished grid part 2:")
